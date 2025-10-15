4d00e3a83382e12453149af8dcbf605d7b48b0f0
RafaelMiillerSilva <rafaelmiillersilva@gmail.com>
2025-10-14 23:30:48 -0300

    removendo comentarios

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,11 @@
 import sys
 from pathlib import Path
 
-# Adiciona backend ao sys.path para imports funcionarem
 ROOT_DIR = Path(__file__).parent.resolve()
 sys.path.append(str(ROOT_DIR))
 
 from PySide6.QtWidgets import QApplication
-from gui.main_window import MainWindow  # agora funciona 100%
+from gui.main_window import MainWindow 
 
 def main():
     app = QApplication(sys.argv)
